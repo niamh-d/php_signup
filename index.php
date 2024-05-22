900b57404e3d857eff3a96794e3bd7429dc4c9e9
Niamh <niamh.linguist@gmail.com>
2024-05-22 12:38:02 +0300

    basic styles

--- index.php
+++ index.php
@@ -1,25 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;600;700&display=swap" rel="stylesheet" />
     <title>Document</title>
 </head>
+
 <body>
 
-<h3>Login</h3>
+    <div class="container">
+        <div class="login">
+            <h3>Login</h3>
 
-<form action="includes/login.inc.php" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <button>Login</button>
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="password" placeholder="Password">
+                <button>Login</button>
+            </form>
+        </div>
+        <div class="signup">
+            <h3>Sign up</h3>
 
-    <h3>Sign up</h3>
-    <form action="includes/signup.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
-        <input type="text" name="email" placeholder="Email"> 
-        <button >Sign up</button>
-    
+            <form action="includes/signup.inc.php" method="post">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="password" placeholder="Password">
+                <input type="text" name="email" placeholder="Email">
+                <button>Sign up</button>
+        </div>
+    </div>
 </body>
+
 </html>
