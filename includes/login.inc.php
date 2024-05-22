524def88eedad354bacb5e29f8f527f74d8dfc3d
Niamh <niamh.linguist@gmail.com>
2024-05-22 20:22:21 +0300

    login logic

---
+++ includes/login.inc.php
@@ -0,0 +1,47 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    try {
+        require_once 'dbh.inc.php';
+        require_once 'login_model.inc.php';
+        require_once 'login_contr.inc.php';
+
+        // ERROR HANDLERS
+
+        $errors = [];
+
+        if (is_input_empty($username, $password)) {
+            $errors["empty_input"] = 'Please fill in all fields.';
+        }
+
+        $user = get_user($pdo, $username);
+
+        if (is_username_wrong($user)) {
+            $errors["login_incorrect"] = 'Incorrect login credentials.';
+        }
+
+        if (!is_username_wrong($user) && is_password_wrong($password, $user['password'])) {
+            $errors["login_incorrect"] = 'Incorrect login credentials.';
+        }
+
+        require_once 'config_session.inc.php';
+
+        if ($errors) {
+            $_SESSION["errors_signup"] = $errors;
+
+            header("Location: ../index.php");
+            die();
+        }
+
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+
+} else {
+    header('Location: ../index.php');
+    die();
+}
