b19aeadc6f448eeff270e5bb464bb795614c89af
Niamh <niamh.linguist@gmail.com>
2024-05-23 11:14:50 +0300

    conditional rendering of forms

--- includes/login_view.inc.php
+++ includes/login_view.inc.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
+function output_logout_link()
+{
+    if (isset($_SESSION['user_id'])) {
+        echo "<a href='includes/logout.inc.php'>Logout</a>";
+    }
+}
+
+function output_username()
+{
+    if (isset($_SESSION['user_id'])) {
+        echo "You are logged in as " . $_SESSION['user_username'] . ".";
+    } else {
+        echo "You are not logged in.";
+    }
+}
+
 function render_login_messages()
 {
     if (isset($_SESSION['errors_login'])) {
--- index.php
+++ index.php
@@ -20,36 +20,45 @@ require_once 'includes/login_view.inc.php';
 <body>
 
     <div class="container">
-        <div class="login">
+        <div>
+        <h3>
+            <?php
+            output_username();
+            ?>
+        </h3>
+        <p>
+        <?php
+        output_logout_link();
+        ?>
+        </p>
+        </div>
+        <?php
+        if (!isset($_SESSION['user_id'])) {
+            echo '<div class="login">
             <h3>Login</h3>
 
             <form action="includes/login.inc.php" method="post">
                 <input type="text" name="username" placeholder="Username">
                 <input type="password" name="password" placeholder="Password">
                 <button>Login</button>
-                <div class="form-messages-container">
-                <?php
-                render_login_messages();
-                ?>
-                </div>
+                <div class="form-messages-container">';
+            render_login_messages();
+            echo '</div>
             </form>
         </div>
         <div class="signup">
             <h3>Sign up</h3>
 
-            <form action="includes/signup.inc.php" method="post">
-                <?php signup_inputs(); ?>
-                <button>Sign up</button>
-                <div class="form-messages-container">
-                <?php
-                render_signup_messages();
-                ?>
-                </div>
+            <form action="includes/signup.inc.php" method="post">';
+            signup_inputs();
+            echo '<button>Sign up</button>
+                <div class="form-messages-container">';
+            render_signup_messages();
+            echo '</div>
             </form>
-        </div>
-        <div>
-            <a href="includes/logout.inc.php">Logout</a>
-        </div>
+        </div>';
+        }
+        ?>
     </div>
 </body>
 
